0d1dfed84508bc12680b07b3bfcf93bfa543ae4d
Kresimir Bratkovic <kreso@selmet.com>
2022-10-27 14:21:35 +0200

    PortfolioFragment.kt init viewModel

--- app/src/main/java/com/kbratkovic/investmentgoldportfolio/ui/addNewItem/AddNewItemFragment.kt
+++ app/src/main/java/com/kbratkovic/investmentgoldportfolio/ui/addNewItem/AddNewItemFragment.kt
@@ -16,12 +16,12 @@ class AddNewItemFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         val view = inflater.inflate(R.layout.fragment_addnewitem, container, false)
-        val slideshowViewModel = ViewModelProvider(this).get(AddNewItemViewModel::class.java)
+        val addNewItemViewModel = ViewModelProvider(this).get(AddNewItemViewModel::class.java)
 
         val itemName: TextInputEditText = view.findViewById(R.id.editTextItemName)
         val textView: TextView = view.findViewById(R.id.text_addNewItem)
 
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
+        addNewItemViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return view
--- app/src/main/java/com/kbratkovic/investmentgoldportfolio/ui/portfolio/PortfolioFragment.kt
+++ app/src/main/java/com/kbratkovic/investmentgoldportfolio/ui/portfolio/PortfolioFragment.kt
@@ -1,38 +1,34 @@
 package com.kbratkovic.investmentgoldportfolio.ui.portfolio
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.textfield.TextInputEditText
 import com.kbratkovic.investmentgoldportfolio.R
+import com.kbratkovic.investmentgoldportfolio.ui.addNewItem.AddNewItemViewModel
 
 //import com.kbratkovic.investmentgoldportfolio.databinding.FragmentHomeBinding
 
-class PortfolioFragment : Fragment(R.layout.fragment_portfolio) {
+class PortfolioFragment : Fragment() {
 
-//    private var _binding: FragmentHomeBinding? = null
-//
-//    // This property is only valid between onCreateView and
-//    // onDestroyView.
-//    private val binding get() = _binding!!
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        val homeViewModel =
-//            ViewModelProvider(this).get(HomeViewModel::class.java)
-//
-//        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-//        val root: View = binding.root
-//
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
-//        return root
-//    }
-//
-//    override fun onDestroyView() {
-//        super.onDestroyView()
-//        _binding = null
-//    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        val view = inflater.inflate(R.layout.fragment_portfolio, container, false)
+
+        val portfolioViewModel = ViewModelProvider(this).get(PortfolioViewModel::class.java)
+
+        val itemName: TextInputEditText = view.findViewById(R.id.editText)
+        val textView: TextView = view.findViewById(R.id.text_home)
+        portfolioViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+
+        return view
+    }
+
+
 }
